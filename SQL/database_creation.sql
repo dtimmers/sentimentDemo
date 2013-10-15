4a9129c824146c858cf200f6eec70686d73e6108
timmers.dennis@gmai.com <timmers.dennis@gmail.com>
2013-10-14 22:05:38 -0700

    improved database design to create tables for tweets, cities and states

--- SQL/database_creation.sql
+++ SQL/database_creation.sql
@@ -1,22 +1,36 @@
-CREATE TABLE Sentiment (
-	id INT NOT NULL AUTO_INCREMENT,
-	datetime DATETIME NOT NULL,
-	txt CHAR(200),
-	cleanTxt CHAR(200),
-	lat FLOAT(7,4),
-	lng FLOAT(7,4),
-	state CHAR(100),
-    primary key (id)
+CREATE TABLE States (
+state_id INT NOT NULL AUTO_INCREMENT,
+name  CHAR(20),
+abr CHAR(2),
+PRIMARY KEY (state_id)
 );
 
-/* Create index on datetime and state for faster searches*/
-CREATE INDEX DSIndex
-ON Sentiment (datetime, state);
+CREATE TABLE Cities (
+city_id INT NOT NULL AUTO_INCREMENT,
+state_id INT,
+city CHAR(20),
+population INT,
+area FLOAT(5,1),
+radius FLOAT(5,4),
+PRIMARY KEY (city_id),
+FOREIGN KEY States(state_id) REFERENCES States(state_id)
+);
 
-/* Creat index on state for faster searches*/
-CREATE INDEX SIndex
-ON Sentiment (state);
+/* Add a foreign key to the Cities table */
+CREATE TABLE Tweets (
+tweet_id INT NOT NULL AUTO_INCREMENT,
+city_id INT,
+datetime DATETIME NOT NULL,
+txt CHAR(200),
+cleanTxt CHAR(200),
+PRIMARY KEY (tweet_id),
+FOREIGN KEY (city_id) REFERENCES Cities(city_id)
+);
 
-/* Create a new user such that R can access the database with tweets 
-   User only has access to the table Sentiment */
-GRANT ALL PRIVILEGES ON sentiment_db.* To 'Ruser'@'localhost' IDENTIFIED BY 'ouRs3cret!';
+/* Create index on datetime and city_id for fast searches*/
+CREATE INDEX dateCityIndex
+ON Tweets (datetime, city_id);
+
+/* Creat index on state for fast searches*/
+CREATE INDEX cityIndex
+ON Tweets (city_id);
