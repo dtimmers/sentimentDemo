da66653056b516ef50cf679fbec655114eb6578c
timmers.dennis@gmai.com <timmers.dennis@gmail.com>
2013-10-15 21:24:14 -0700

    added uniqueness constraints to tables

--- SQL/database_creation.sql
+++ SQL/database_creation.sql
@@ -1,11 +1,11 @@
-CREATE TABLE States (
+CREATE TABLE state (
 state_id INT NOT NULL AUTO_INCREMENT,
-name  CHAR(20),
+name  CHAR(20) UNIQUE,
 abr CHAR(2),
 PRIMARY KEY (state_id)
 );
 
-CREATE TABLE Cities (
+CREATE TABLE city (
 city_id INT NOT NULL AUTO_INCREMENT,
 state_id INT,
 city CHAR(20),
@@ -13,24 +13,32 @@ population INT,
 area FLOAT(5,1),
 radius FLOAT(5,4),
 PRIMARY KEY (city_id),
-FOREIGN KEY (state_id) REFERENCES States(state_id)
+FOREIGN KEY (state_id) REFERENCES state(state_id)
 );
 
+ALTER TABLE city 
+ADD CONSTRAINT uc_city UNIQUE (state_id,city)
+
 /* Add a foreign key to the Cities table */
-CREATE TABLE Tweets (
+CREATE TABLE tweet (
 tweet_id INT NOT NULL AUTO_INCREMENT,
 city_id INT,
 datetime DATETIME NOT NULL,
 txt CHAR(200),
 cleanTxt CHAR(200),
+score FLOAT(5,2),
+screenName CHAR(20),
 PRIMARY KEY (tweet_id),
-FOREIGN KEY (city_id) REFERENCES Cities(city_id)
+FOREIGN KEY (city_id) REFERENCES city(city_id)
 );
+
+ALTER TABLE tweet 
+ADD CONSTRAINT uc_tweet UNIQUE (datetime,screenName)
 
 /* Create index on datetime and city_id for fast searches*/
 CREATE INDEX dateCityIndex 
-ON Tweets (datetime, city_id);
+ON tweet (datetime, city_id);
 
 /* Creat index on state for fast searches*/
 CREATE INDEX cityIndex 
-ON Tweets (city_id);
+ON tweet (city_id);
