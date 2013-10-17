ba71ace171453f80b534b9a8639a0b0a62dc554f
timmers.dennis@gmai.com <timmers.dennis@gmail.com>
2013-10-16 23:19:04 -0700

    updated commands to include lattitude and longitude in the city table

--- R/sql_commands.R
+++ R/sql_commands.R
@@ -4,28 +4,46 @@ pw <- 'ouRs3cret!'
 db_name <- 'sentiment_db'
 
 # Opens a connection to the database and sends a query
-send_query <- function(cmd, db_name='sentiment_db'){
-  con <- dbConnect( MySQL(), user=usr, password=pw, dbname=db_name, host='localhost')
+send_query <- function(cmd, db_name='sentiment_db', con=NULL){
+  start_con <- F
+  if(is.null(con)){
+    start_con <- T
+    con <- dbConnect( MySQL(), user=usr, password=pw, dbname=db_name, host='localhost')
+  }
   tryCatch(dbSendQuery(con, cmd),
            error=function(msg) {
              message(cat(paste(msg)))
            },
+           warning=function(msg) {
+             message(cat(paste(msg)))
+             res <- NULL
+           },
            finally={
-             dbDisconnect(con)
+             if( start_con ){
+               dbDisconnect(con)
+             }
            }
   )
 }
 
-get_query <- function(cmd, db_name='sentiment_db'){
-  con <- dbConnect( MySQL(), user=usr, password=pw, dbname=db_name, host='localhost')
+get_query <- function(cmd, db_name='sentiment_db', con=NULL){
+  if(is.null(con)){
+    con <- dbConnect( MySQL(), user=usr, password=pw, dbname=db_name, host='localhost')
+  }
   res <- tryCatch({
     res <- dbGetQuery(con, cmd)},
     error=function(msg) {
       message(cat(paste(msg)))
       res <- NULL
     },
+    warning=function(msg) {
+      message(cat(paste(msg)))
+      res <- NULL
+    },
     finally={
-      dbDisconnect(con)
+      if( is.null(con) ){
+        dbDisconnect(con)
+      }
     }
   )
   return(res)
@@ -38,7 +56,6 @@ sql_initialize_tables <- function(){
   for( sql_cmd in cmd_vec){
     send_query(sql_cmd)
   }
-  cat(paste('Successfully created all the necessary tables on the database', db_name))
 }
 
 sql_fill_state <- function(tab='state'){
@@ -50,32 +67,38 @@ sql_fill_state <- function(tab='state'){
 
 sql_fill_city <- function(tab='city'){
   load('data/state_info.RData')
-  df <- state_info[, c('Abr', 'City', 'Population', 'Land.Area.in.Square.Miles', 'radius')]
+  df <- state_info[, c('Abr', 'City', 'Population', 'Land.Area.in.Square.Miles', 
+                       'radius', 'lat', 'lng')]
   df <- df_val_to_string(df, c('Abr', 'City'))
+  con <- dbConnect( MySQL(), user=usr, password=pw, dbname=db_name, host='localhost')
+  str(con)
   n <- nrow(df)
   for( i in 1:n ){
     row <- df[i,]
     if ( sum(is.na(row))==0 ){
-      cmd <- 'INSERT INTO city (state_id, city, population, area, radius) SELECT state_id,'
+      cmd <- 'INSERT INTO city (state_id, city, population, area, radius, lat, lng) 
+      SELECT state_id,'
       cmd <- paste(cmd, paste(row[-1], collapse=', '))
       cmd <- paste(cmd, ' FROM state WHERE abr=', row[1],';', sep='')
-      send_query(cmd)
-    }    
+      send_query(cmd, con=con)
+    }
   }
+  dbDisconnect(con)
 }
 
 sql_insert_tweet_df <- function(tw_df){
   Ntweet <- nrow(tw_df)
   cols <- c('city_id', 'created', 'text', 'cleanText', 'score', 'screenName')
-  #for(i in 1:Ntweet){
-  for(i in 1:1){
+  con <- dbConnect( MySQL(), user=usr, password=pw, dbname=db_name, host='localhost')
+  for(i in 1:Ntweet){
     tweet <- tw_df[i, cols]
     tweet <- df_val_to_string(tweet, c('created', 'text', 'cleanText', 'screenName'))
     cmd <- 'INSERT INTO tweet (city_id, datetime, txt, cleanTxt, score, screenName) VALUES'
     cmd <- paste(cmd, paste('(',paste(tweet, collapse=","),');'))
     print(cmd)
-    send_query(cmd)
+    send_query(cmd, con=con)
   }
+  dbDisconnect(con)
 }
 
 df_val_to_string <- function(df, str_cols=NULL){
