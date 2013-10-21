8661d2b58800f44de6ce3b2d9dbbbec32ca46d05
timmers.dennis@gmai.com <timmers.dennis@gmail.com>
2013-10-20 20:44:11 -0700

    added a post method for sentiment and removed redundant pages

--- Python/mainpage.py
+++ Python/mainpage.py
@@ -1,16 +1,15 @@
+import sys
 import os
 import jinja2
 import webapp2
+from re import search as regexp_search
 
-from google.appengine.ext.webapp import template
 from google.appengine.ext import db
 import figuresDB
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'])
-
-menu = [ ('Home','/'), ('Sentiment','/sentiment'), ('Post Figures', '/postfigures') ]
 
 class MainPage(webapp2.RequestHandler):
 
@@ -26,8 +25,7 @@ class MainPage(webapp2.RequestHandler):
         content = '<h3> Welcome to my Google App Engine website.</h3>\n'
         content += '</br>\n'
         content += 'I have been learning Python and decided to publish some of my apps on the Google App Engine. '
-        content += 'So far I have published an app which can be used as a feature selection tool. '
-        content += 'I am working on extending this tool and over time will publish more tools. </p>'
+        content += 'The first app is a sentiment app I created using the programming language R. </p>'
 
         self.response.write( content )
 
@@ -49,29 +47,101 @@ class Sentiment(webapp2.RequestHandler):
         self.response.write(head_tmpl.render())
 
         menu_tmpl = JINJA_ENVIRONMENT.get_template('templates/menu.html')
-        self.response.write(menu_tmpl.render({'active': active}))   
+        self.response.write(menu_tmpl.render({'active': active}))
 
 
-        content = '<h3> Who gets the blame: Republicans or Democrats?</h3>\n'
-        content += '</br>\n'
-        content += '<p>During the 2013 shutdown we collected tweets which have either a democratic or republican hashtag during the 2013 shutdown. '
-        content += 'Over a period of time we did a simple sentiment analysis of these tweets i.e. we test whether the tweets are positive or negative. '
-        content += 'The sentiment scores are graphically displayed in a heatmap of the USA. '
-        content += 'Furthermore, every day we also created a word cloud out of these tweets.</p>'
+        content = "<p> I wrote a program in R which downloads tweets from Twitter on a given topic (tried 'obamacare' and 'biking' sofar). These tweets are used to do a sentiment analysis on the given topics usig the <a href='http://mpqa.cs.pitt.edu/lexicons/subj_lexicon/' target='_blank'> MPQA Subjectivity Lexicon </a>. The Subjectivity Lexicon is a list of words which are scored between -1 and +1. A score of -1 corresponds to a negative sentiment while +1 corresponds to a positive sentiment. By downloading tweets in every state of the US and using the Subjectivity Lexicon to compute the average score, you get a sentiment score per topic per state. The average scores are mapped into a heatmap of the US.</p>"
 
         content += '<h4> Heatmap of the states </h4>\n'
 
         self.response.write( content )
 
-        self.response.write( "<div class=centered>")
+        q_term = db.GqlQuery("SELECT DISTINCT search_term FROM Figure")
 
-        self.response.write("<img id=heatmap class=centered width=600 src=img/sentiment/sentiment01.png>")
+        options = []
+        for f in q_term:
+            options.append({'value': f.search_term.replace(" ","#"), 'name': f.search_term})
+        active_topic = options[0]['name']
 
-        self.response.write("<img id=repCloud class=left width=290 src=img/sentiment/bigdata01.png>")
+        select_tmpl = JINJA_ENVIRONMENT.get_template('templates/select_term.html')
+        self.response.write(select_tmpl.render({'options': options, 'active': active_topic}))
 
-        self.response.write("<img id=repCloud class=right width=290 src=img/sentiment/bigdata01.png>")
+        # select all sentiment figures from the active topic
+        q_sentfigs = db.GqlQuery("SELECT * FROM Figure WHERE search_term = :search_term ORDER BY date", search_term=active_topic)
 
+        
+        active_id = q_sentfigs[0].search_term + q_sentfigs[0].date
+        
+        for f in q_sentfigs:
+            fig_id = f.search_term + f.date
+            img_tmpl = JINJA_ENVIRONMENT.get_template('templates/sent_img.html')
+            self.response.write(img_tmpl.render({'figurekey': f.key(), 'active': active_id, 'id':fig_id}))
+
+        self.response.write("""<div class="buttonWrapper">""")
+        for f in q_sentfigs:
+            value = f.search_term + f.date
+            radio_tmpl = JINJA_ENVIRONMENT.get_template('templates/sent_radio.html')
+            self.response.write(radio_tmpl.render({'value': value, 'checked':active_id, 'date':f.date}))
         self.response.write("</div>")
+
+        
+
+        end_content_tmpl = JINJA_ENVIRONMENT.get_template('templates/end_content.html')
+        self.response.write(end_content_tmpl.render())
+
+        sidebar_tmpl = JINJA_ENVIRONMENT.get_template('templates/sidebar.html')
+        self.response.write(sidebar_tmpl.render())
+
+        footer_tmpl = JINJA_ENVIRONMENT.get_template('templates/footer.html')
+        self.response.write(footer_tmpl.render())
+
+    def post(self):
+        active = 'sentiment'
+        active_topic = self.request.get('searchTerm').replace('#', ' ')
+        print(active_topic)
+
+        head_tmpl = JINJA_ENVIRONMENT.get_template('templates/header.html')
+        self.response.write(head_tmpl.render())
+
+        menu_tmpl = JINJA_ENVIRONMENT.get_template('templates/menu.html')
+        self.response.write(menu_tmpl.render({'active': active}))
+
+
+        content = "<p> I wrote a program in R which downloads tweets from Twitter on a given topic (tried 'obamacare' and 'biking' sofar). These tweets are used to do a sentiment analysis on the given topics usig the <a href='http://mpqa.cs.pitt.edu/lexicons/subj_lexicon/' target='_blank'> MPQA Subjectivity Lexicon </a>. The Subjectivity Lexicon is a list of words which are scored between -1 and +1. A score of -1 corresponds to a negative sentiment while +1 corresponds to a positive sentiment. By downloading tweets in every state of the US and using the Subjectivity Lexicon to compute the average score, you get a sentiment score per topic per state. The average scores are mapped into a heatmap of the US.</p>"
+
+        content += '<h4> Heatmap of the states </h4>\n'
+
+        self.response.write( content )
+
+        q_term = db.GqlQuery("SELECT DISTINCT search_term FROM Figure")
+
+        options = []
+        for f in q_term:
+            options.append({'value': f.search_term.replace(" ","#"), 'name': f.search_term})
+
+        select_tmpl = JINJA_ENVIRONMENT.get_template('templates/select_term.html')
+        self.response.write(select_tmpl.render({'options': options, 'active': active_topic}))
+
+        # select all sentiment figures from the active topic
+
+        q_sentfigs = db.GqlQuery("SELECT * FROM Figure WHERE search_term = :search_term ORDER BY date", search_term=active_topic)
+
+        
+        active_id = q_sentfigs[0].search_term + q_sentfigs[0].date
+        
+        for f in q_sentfigs:
+            fig_id = f.search_term + f.date
+            img_tmpl = JINJA_ENVIRONMENT.get_template('templates/sent_img.html')
+            self.response.write(img_tmpl.render({'figurekey': f.key(), 'active': active_id, 'id':fig_id}))
+
+        self.response.write("""<div class="buttonWrapper">""")
+        for f in q_sentfigs:
+            value = f.search_term + f.date
+            radio_tmpl = JINJA_ENVIRONMENT.get_template('templates/sent_radio.html')
+            self.response.write(radio_tmpl.render({'value': value, 'checked':active_id, 'date':f.date}))
+        self.response.write("</div>")
+
+        
 
         end_content_tmpl = JINJA_ENVIRONMENT.get_template('templates/end_content.html')
         self.response.write(end_content_tmpl.render())
@@ -86,7 +156,6 @@ class PostFigures(webapp2.RequestHandler):
 
     def get(self):
         active = 'postfigures'
-        figureform = model_form(figuresDB.FigureDetails)()
 
         head_tmpl = JINJA_ENVIRONMENT.get_template('templates/header.html')
         self.response.write(head_tmpl.render())
@@ -94,49 +163,8 @@ class PostFigures(webapp2.RequestHandler):
         menu_tmpl = JINJA_ENVIRONMENT.get_template('templates/menu.html')
         self.response.write(menu_tmpl.render({'active': active}))
 
-        self.response.write(template.render('templates/figure_form.html', 
-            {'form': figureform, 'sub_value': 'Submit the figure'}))
-
-
-        end_content_tmpl = JINJA_ENVIRONMENT.get_template('templates/end_content.html')
-        self.response.write(end_content_tmpl.render())
-
-        sidebar_tmpl = JINJA_ENVIRONMENT.get_template('templates/sidebar.html')
-        self.response.write(sidebar_tmpl.render())
-
-        footer_tmpl = JINJA_ENVIRONMENT.get_template('templates/footer.html')
-        self.response.write(footer_tmpl.render())  
-
-class FeatureSelector(webapp2.RequestHandler):
-
-    def get(self):        
-        active = 'Feature Selection'
-
-        head_tmpl = JINJA_ENVIRONMENT.get_template('templates/header.html')
-        self.response.write(head_tmpl.render())
-
-        menu_tmpl = JINJA_ENVIRONMENT.get_template('templates/menu.html')
-        self.response.write(menu_tmpl.render({'menu': menu, 'active': active}))
-
-        content = '<h3> Feature Selection Tool </h3>'
-        content += '<p> The Feature Selection Tool uses linear regression and logistic regression to visualize the most important features. In particular it computes a significance level for each feature. These scores are visualized in a bar chart: the higher the bar the more significant the feature (the levels are plotted on a log-scale). The linear regression and logistic regression scores are combined to get a more powerful prediction.'
-        content += "You can <a href='/featureselectorcustom'>upload</a> your own data or find a more <a href='featureselectorexplain'> elaborate explanation </a> about the significance levels.</p>"
-
-        self.response.write( content )
-
-        script = "<script type='text/javascript'>function swapPlot(){var image = document.getElementById('significancePlot');var dropd = document.getElementById('plotSelect');image.src = dropd.value;};</script>"
-
-        self.response.write(script)
-
-        options = []
-        options.append({'value':'img/OLS_pvals.png', 'name': 'Linear Score'})
-        options.append({'value':'img/Logit_pvals.png', 'name': 'Logistic Score'})
-        options.append({'value':'img/Overall_pvals.png','name': 'Combined Score'})
-
-        self.response.write("<a id='plot'></a>")
-        select_tmpl = JINJA_ENVIRONMENT.get_template('templates/select.html')
-        self.response.write(select_tmpl.render( {'options': options } ))
-        self.response.write("<img id=significancePlot class=centered src=img/OLS_pvals.png>")
+        figure_form_tmpl = JINJA_ENVIRONMENT.get_template('templates/figure_form.html')
+        self.response.write(figure_form_tmpl.render())
 
         end_content_tmpl = JINJA_ENVIRONMENT.get_template('templates/end_content.html')
         self.response.write(end_content_tmpl.render())
@@ -146,72 +174,35 @@ class FeatureSelector(webapp2.RequestHandler):
 
         footer_tmpl = JINJA_ENVIRONMENT.get_template('templates/footer.html')
         self.response.write(footer_tmpl.render())
-
-class FeatureSelectorCustom(webapp2.RequestHandler):
-
-    def get(self):        
-        active = 'Feature Selection'
-
-        head_tmpl = JINJA_ENVIRONMENT.get_template('templates/header.html')
-        self.response.write(head_tmpl.render())
-
-        menu_tmpl = JINJA_ENVIRONMENT.get_template('templates/menu.html')
-        self.response.write(menu_tmpl.render({'menu': menu, 'active': active}))
-
-        content = '<h3> Feature Selection Tool </h3>'
-        content += '<p> The Feature Selection Tool uses linear regression and logistic regression to visualize the most important features. These significance levels can be combined to obtain an Combined Significance score. The significance of each variable is computed and displayed in a chart bar. The taller the bar the more significant the variable. </p>'
-
-        self.response.write( content )
-
-        csv_form_tmpl  = JINJA_ENVIRONMENT.get_template('templates/csv_form.html')
-        self.response.write(csv_form_tmpl.render())
-
-        end_content_tmpl = JINJA_ENVIRONMENT.get_template('templates/end_content.html')
-        self.response.write(end_content_tmpl.render())
-
-        sidebar_tmpl = JINJA_ENVIRONMENT.get_template('templates/sidebar.html')
-        self.response.write(sidebar_tmpl.render())
-
-        footer_tmpl = JINJA_ENVIRONMENT.get_template('templates/footer.html')
-        self.response.write(footer_tmpl.render())        
 
     def post(self):
-        active = 'Feature Selection'
+        active = 'postfigures'
+        file_list = self.request.get('figures')
+
         head_tmpl = JINJA_ENVIRONMENT.get_template('templates/header.html')
         self.response.write(head_tmpl.render())
 
         menu_tmpl = JINJA_ENVIRONMENT.get_template('templates/menu.html')
-        self.response.write(menu_tmpl.render({'menu': menu, 'active': active}))
+        self.response.write(menu_tmpl.render({'active': active}))
 
-        y = []
-        X = []
-        try:
-            data = self.request.get('csvfile').split('\n')
-            for line in data:
-                data_entry = line.strip().split(',')
-                y.append(data_entry.pop(-1))
-                X.append([float(x) for x in data_entry])
-        except ValueError as verr:
-                float_msg = "Problem occurred while importing the data.\\nNot all data could be converted to floats:\\n    " + str(verr)
-                self.response.write('<script type="text/javascript">alert("' + float_msg + '");</script>')
-                self.redirect("/featureselectorcustom")
+        for file_data in self.request.POST.getall('figures'):
+            new_figure = figuresDB.Figure()
         
-        # fs = fsmod.FeatureSelection(X,y)
-        # fs.createPlots()
-        
+            plot_type = regexp_search('^\w+(?=-)', file_data.filename).group(0)
+            new_figure.plot_type = plot_type
+            
+            term = regexp_search('(?<=-)(\w\s*)+(?=-)', file_data.filename).group(0)
+            new_figure.search_term = term
 
-        content = '<h3> Feature Selection </h3>'
-        self.response.write( content )
+            # TODO: change this into an honest date instead of text
+            day = regexp_search('(?<=-)[0-9]{4}-[0-9]{2}-[0-9]{2}(?=.)', file_data.filename).group(0)
+            new_figure.date = day
 
-        options = []
-        options.append({'value':'img/custom_OLS_pvals.png', 'name': 'Linear Score'})
-        options.append({'value':'img/custom_Logit_pvals.png', 'name': 'Logistic Score'})
-        options.append({'value':'img/custom_Overall_pvals.png','name': 'Combined Score'})
+            new_figure.figure = file_data.value
 
-        self.response.write("<a id='plot'></a>")
-        select_tmpl = JINJA_ENVIRONMENT.get_template('templates/select.html')
-        self.response.write(select_tmpl.render( {'options': options } ))
-        self.response.write("<img id=significancePlot class=centered src=img/custom_OLS_pvals.png>")
+            #new_figure.put()
+
+            self.response.write("Successfully uploaded " + file_data.filename + "</br>")
 
         end_content_tmpl = JINJA_ENVIRONMENT.get_template('templates/end_content.html')
         self.response.write(end_content_tmpl.render())
@@ -221,11 +212,19 @@ class FeatureSelectorCustom(webapp2.RequestHandler):
 
         footer_tmpl = JINJA_ENVIRONMENT.get_template('templates/footer.html')
         self.response.write(footer_tmpl.render())
+
+class GetImage(webapp2.RequestHandler):
+    def get(self):
+        fig = db.get(self.request.get('entity_id'))
+        if fig.figure:
+            self.response.headers['Content-Type'] = "image/png"
+            self.response.out.write(fig.figure)
 
 
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/sentiment', Sentiment),
-    ('/postfigures', PostFigures)
+    ('/postfigures', PostFigures),
+    ('/img', GetImage)
 ], debug=True)
