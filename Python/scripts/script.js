427e73746140330e02de9ca82d2b5966f77b4f23
timmers.dennis@gmai.com <timmers.dennis@gmail.com>
2013-10-20 20:44:51 -0700

    added functions for switching between dates and search terms

--- Python/scripts/script.js
+++ Python/scripts/script.js
@@ -1,40 +1,15 @@
-$( document ).ready(function() {
+$(document).ready(function($) {
+	$("input[name='figButton']").change(function() {
+            var value = "img[id='" + $(this).val() + "']";
+            $('.active_sentiment').removeClass('active_sentiment').addClass('nonactive_sentiment');
+            $(value).removeClass('nonactive_sentiment').addClass('active_sentiment');
+        });
 
-	$("#figures").change(function(){
-		console.log($('#figures'));
-		console.log(this.files);
-		/*var fn = $("#figure").val().replace(/C:\\fakepath\\/i, '');
-		console.log(fn)*/
+	$('#term_form').change(function() {
+		var value = $('#termSelect').val();
+		var name = $('#termSelect')
+		var input = $("<input>").attr("type", "hidden").attr("name", "searchTerm").val(value);
+		$(this).append($(input));
+		$(this).submit();
 	});
 });
-
-/*var extTypes= ['csv', 'txt'];
-
-function validateForm(formElement) {
-	var path = formElement.csvfile.value;
-	if (path.length > 3) {
-		var ext = path.substr(path.length - 3);
-		if ( extTypes.indexOf(ext) >= 0 ) {
-			return true;
-		}
-		else {
-			alert("The file must either be a txt or csv file");
-			return false;
-		}
-	}
-	else {
-		alert("The file must either be a txt or csv file");
-		return false;
-	}
-	
-};
-
-function swapPlot() {
-	var image = document.getElementById('significancePlot');
-	var dropd = document.getElementById('plotSelect');
-	image.src = dropd.value;
-};*/
-
-/*	$('#name').attr("disabled", true);
-	$('#date').attr("disabled", true);
-	$('#search_term').attr("disabled", true);*/
