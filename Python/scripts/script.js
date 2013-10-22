ceb19f5fb8317beec80a525c4902caa19925444a
timmers.dennis@gmai.com <timmers.dennis@gmail.com>
2013-10-21 22:57:31 -0700

    added jQuery function to disable submit button when no files are selected

--- Python/scripts/script.js
+++ Python/scripts/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function($) {
+	/*  Switch the sentiment images using the radio buttons */
 	$("input[name='figButton']").change(function() {
             var value = "img[id='" + $(this).val() + "']";
             console.log(value)
@@ -6,10 +7,18 @@ $(document).ready(function($) {
             $(value).removeClass('nonactive_sentiment').addClass('active_sentiment');
         });
 
+	/* To send correct post while there is no submit button */
 	$('#term_form').change(function() {
 		var value = $('#termSelect').val();
 		var input = $("<input>").attr("type", "hidden").attr("name", "searchTerm").val(value);
 		$(this).append($(input));
 		$(this).submit();
+	});
+
+	/* Check whether the list of files is not empty */
+	$('#figures').bind('change', function() {
+		if( $(this)[0].files.length > 0 ){
+			$('#upFigures').removeAttr('disabled');
+		};
 	});
 });
